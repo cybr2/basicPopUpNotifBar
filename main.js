6ea7a369b460bd29ca587966f9f8aca9edf8fddf
cybr2 <ydoowbernisca2@gmail.com>
2023-11-09 04:02:54 +0800

    final output

---
+++ main.js
@@ -0,0 +1,11 @@
+const popUpBox = document.querySelector('.popUp');
+
+
+
+
+const openPopUp = () => {
+    popUpBox.classList.add('open-popUp');
+}
+const closePopUp = () => {
+    popUpBox.classList.remove('open-popUp');
+}
